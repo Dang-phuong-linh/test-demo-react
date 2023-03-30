28347d278690e5abd66721d016edecee82836233
Dang-phuong-linh <phuonglinh26112003@gmail.com>
2023-03-31 01:50:52 +0700

    form vs react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -24,19 +24,28 @@ class MyComponent extends React.Component {
   handleonMoverOver(event) {
     // console.log(event.pageX);
   }
+
+  handleOnChangeInput = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state); //in ra obj cua state
+  };
   //jsx
   render() {
     return (
       <div>
         My name is {this.state.name} and i'm {this.state.age}
-        <button onMouseOver={this.handleonMoverOver}>Hover me</button>
-        <button
-          onClick={(event) => {
-            this.handleClick(event);
-          }}
-        >
-          Click me
-        </button>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnChangeInput(event)}
+          />
+          <button>submit</button>
+        </form>
       </div>
     );
   }
