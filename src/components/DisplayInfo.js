e4d9daa2c33a32c4a41adc259b5d2fa09fceda0e
Dang-phuong-linh <phuonglinh26112003@gmail.com>
2023-04-02 23:30:42 +0700

    useEffect

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,48 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfo.scss";
 // import logo from "./../logo.svg";
 
 //stateless vs stateful
-// class DisplayInfo extends React.Component {
-//   render() {
-//     console.log(">>>call me render");
-//     //destructuring array
-//     const { listUsers } = this.props;
-//     console.log(listUsers);
-//     //props = vt tat property
-
-//     //template + logic.js
-//     return (
-//       <div className="display-info-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user, index) => {
-//               console.log(user);
-//               console.log(">>> Check map user", user);
-
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>
-//                     <div>My name is {user.name}</div>
-//                     <div>My age is {user.age}</div>
-//                   </div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfo = (props) => {
   const { listUsers } = props;
@@ -56,6 +16,12 @@ const DisplayInfo = (props) => {
     // alert("click me");
     setShowHideListUser(!isShowHideListUser);
   };
+  console.log("call me render");
+  useEffect(() => {
+    if (listUsers.length === 0) alert("you delete all user");
+    console.log(">>>call me useEffect");
+  }, [listUsers]);
+
   return (
     <div className="display-info-container">
       <div>
