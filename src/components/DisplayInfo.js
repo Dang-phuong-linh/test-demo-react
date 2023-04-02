0cb309ad7d191d974e38f47daa0270b886b149d6
Dang-phuong-linh <phuonglinh26112003@gmail.com>
2023-04-02 22:05:33 +0700

    useState

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfo.scss";
 // import logo from "./../logo.svg";
 
@@ -47,9 +47,23 @@ import "./DisplayInfo.scss";
 const DisplayInfo = (props) => {
   const { listUsers } = props;
 
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+  // this.state = {
+  //   isShowHideListUser: true,
+  // };
+
+  const handleShowHideListUser = () => {
+    // alert("click me");
+    setShowHideListUser(!isShowHideListUser);
+  };
   return (
     <div className="display-info-container">
-      {true && (
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? "hide list users" : "show list user"}
+        </span>
+      </div>
+      {isShowHideListUser && (
         <>
           {listUsers.map((user, index) => {
             console.log(user);
