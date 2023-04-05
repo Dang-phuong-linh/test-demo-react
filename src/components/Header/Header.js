579dc321f241ffc8d11097127540e50e217fdb94
Dang-phuong-linh <phuonglinh26112003@gmail.com>
2023-04-05 11:12:28 +0700

    css app container

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,11 +31,13 @@ const Header = () => {
           </Nav>
 
           <nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+            <button className="btn-login">Log in</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </nav>
         </Navbar.Collapse>
       </Container>
